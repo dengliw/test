8a46ba9e2adbb8f29c8688bab1f75008c6dcb4c0
LiWei Deng <denglw0600@qq.com>
2017-10-17 20:54:43 +0800

    今日头条

---
+++ 笔试/今日头条/头条1.java
@@ -0,0 +1,48 @@
+﻿import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+
+/*
+******************************开始写代码******************************/
+    static int solve(String[] a,int n, int m) {
+        int b = 0;
+		//int len_1 = a.length;
+        //int len_2 = a[0].length;
+        char[][] ch = new char[n][m];
+        for(int i = 0; i < a.length; i++){
+            ch[i] = a[0].toCharArray();//转化为字符数组
+            
+        }
+       // int 
+       /* for( i = 0; i < a.length; i++){//遍历数组，找出各初始位置
+            
+        //}*/
+		return -1;
+    }
+/******************************结束写代码******************************/
+
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int res;
+            
+        int n = 0;
+        n = Integer.parseInt(in.next().trim());
+        int m = 0;
+        m = Integer.parseInt(in.next().trim());
+        String[] _a = new String[n];
+        int _a_item;
+        for(int i = 0; i < n; i++) {
+            _a[i] = in.nextLine().trim();
+        }
+  		
+        res = solve(_a,n,m);
+        System.out.println(res);
+
+    }
+}
---
+++ 笔试/今日头条/头条2.java
@@ -0,0 +1,84 @@
+﻿import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+
+/*
+******************************开始写代码******************************/
+    static int[] solve(int[] a,int x) {
+        x--;
+		int[] b = new int[a.length];
+        int sign = 0;//记录最小的下标
+       	int le = b[0];//初始化。
+        for(int i = 0; i < a.length; i++){
+            if(le > a[i]){
+                le = a[i];
+                sign = i;
+            }
+        }
+        int s = 0;//距离
+        if( x == sign){
+            for( int i = 0; i < b.length; i++){
+                b[i] = a[i] - le ;
+            }
+            b[sign] = le * b.length;
+            return b;
+        }
+        else if(x < sign){//如果结束下标在最小元素左边
+            s = b.length - sign + x;
+            for( int i = 0; i < b.length; i++){
+                b[i] = a[i] - le -1;
+                if( i < sign && i > x){
+                	b[i] = a[i] - le;
+                }
+            }
+            b[sign] = le*b.length + s;
+            return b;
+        }
+        else if( x > sign)
+        {
+            
+            s = x - sign + 1;
+            b[sign] = le * b.length + s;
+            for( int i = 0; i < sign; i++){
+                b[i] = a[i] - le;
+            }
+            for( int i = sign + 1; i <= x; i++){
+                b[i] = a[i] - le -1;
+            }
+            return b;
+            
+        }
+        
+		return b;
+    }
+/******************************结束写代码******************************/
+
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int[] res;
+            
+        int _a_size = 0;
+        _a_size = Integer.parseInt(in.next().trim());
+        int _a_num = 0;
+        _a_num = Integer.parseInt(in.next().trim());
+        int[] _a = new int[_a_size];
+        int _a_item;
+        for(int _a_i = 0; _a_i < _a_size; _a_i++) {
+            _a_item = Integer.parseInt(in.next().trim());
+            _a[_a_i] = _a_item;
+        }
+  
+        res = solve(_a,_a_num);
+        System.out.println(res);
+       for(int res_i=0; res_i < res.length; res_i++) {
+            System.out.println(String.valueOf(res[res_i]));
+        }
+
+    }
+}
